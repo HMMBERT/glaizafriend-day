cb35a64ecf11674660e78a8ad7afa281b35d5f89
REB <reb@REB.REB>
2025-08-01 16:57:53 +0800

    Header Fix

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,7 @@ function Header() {
   return (
     <header className="header">
       <h1 className="header-title">Happy Girlfriend Day!</h1>
-      <br></br>     <br></br>     <br></br>
+     
       <p className="header-subtitle">To my Bilo Bilo</p>
     </header>
   );
